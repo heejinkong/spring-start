8ebfb7899798b756cfec7869fc2a8b1b657494ec
heejinKong <gmlwls3379@naver.com>
2025-01-18 23:10:17 +0900

    spring

--- hello-spring/src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package hello_spring.hello_spring;
+package hello.hello_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +11,4 @@ public class HelloSpringApplication {
 	}
 
 }
+ 
--- hello-spring/src/test/java/hello/hello_spring/HelloSpringApplicationTests.java
+++ hello-spring/src/test/java/hello/hello_spring/HelloSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package hello_spring.hello_spring;
+package hello.hello_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
