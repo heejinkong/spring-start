81b226f6c59c9483066fe943fa76d87e4e2e63a2
heejinKong <gmlwls3379@naver.com>
2025-02-03 16:23:43 +0900

    JPA Auditing 적용

--- memo/src/main/java/com/sparta/memo/entity/Memo.java
+++ memo/src/main/java/com/sparta/memo/entity/Memo.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 @Table(name = "memo") // 매핑할 테이블의 이름을 지정
 @NoArgsConstructor // 기본 생성자 생성
-public class Memo {
+public class Memo extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
